dbb4d0cd3c593a073291656964ffa0c361c4ce32
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 19:55:25 -0400

    list item with list view

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -1,24 +1,28 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 
 class ListView1Screen extends StatelessWidget {
-  final List<String> options;
+  final List<String> options = const [
+    'Megaman',
+    'Meta Gear',
+    'Super Smash',
+    'Genshin',
+    'Minecraft',
+  ];
 
-  const ListView1Screen({super.key, required this.options});
+  const ListView1Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Listview Tipo 1')),
-      body: ListView(
-        children: [
-          ...options
-              .map((e) => ListTile(
-                    title: Text(e),
-                    trailing: const Icon(Icons.arrow_forward_ios_outlined),
-                  ))
-              .toList(),
-        ],
-      ),
+      body: ListView.separated(
+          itemBuilder: (context, index) {
+            return Text('${options[index]} $index');
+          },
+          separatorBuilder: (context, index) => const Divider(),
+          itemCount: options.length),
     );
   }
 }
