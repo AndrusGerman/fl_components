ebf4e3e3f710f352454da217cf0c0252fa496f1e
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 20:01:19 -0400

    list view color style

--- lib/screens/listview1_screen.dart
+++ lib/screens/listview1_screen.dart
@@ -16,15 +16,19 @@ class ListView1Screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Listview Tipo 1')),
+      appBar: AppBar(
+        title: const Text('Listview Tipo 1'),
+        elevation: 0,
+        backgroundColor: Colors.indigo,
+      ),
       body: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
-               title:  Text('${options[index]} $index'),
-               trailing: const Icon(Icons.arrow_forward_ios),
-               onTap: () {
-                 print('Click on ${index}');
-               },
+              title: Text('${options[index]} $index'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {
+                print('Click on ${index}');
+              },
             );
           },
           separatorBuilder: (context, index) => const Divider(),
