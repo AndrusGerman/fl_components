886f523b0aefb5cdac6f90cb2c4cdad58d92970c
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 23:50:03 -0400

    custom card type, image placeholder style

--- lib/widgets/custom_card_type2.dart
+++ lib/widgets/custom_card_type2.dart
@@ -6,20 +6,30 @@ class CustomCardType2 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,// Evita que los elementos se salgan, como las imagenes
       elevation: 10,
       child: Column(
-        children: const [
-          FadeInImage(
+        children: [
+          const FadeInImage(
             image: NetworkImage(
                 'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_4x3.jpg'),
             placeholder: AssetImage('assets/images/jar-loading.gif'),
-
-            width: double.infinity,
+           // width entero
+            width: double.infinity, 
             height: 230,
-            fit:BoxFit.cover,
-
-
+            // llena toda la imagen
+            fit: BoxFit.cover,
+            // animation view image
             fadeInDuration: Duration(milliseconds: 300),
+          ),
+          Container(
+            alignment: AlignmentDirectional.centerEnd,
+            padding: const EdgeInsets.only(
+              right: 20,
+              top: 10,
+              bottom: 10
+            ),
+            child: const Text('un hermoso paisaje'),
           )
         ],
       ),
