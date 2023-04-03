8a11b455dbbc804f5e2b47a042019b0c94643d10
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-03 00:00:49 -0400

    list images widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -19,7 +19,23 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardType1(),
             SizedBox(height: 10,),
-            CustomCardType2(),
+            CustomCardType2(imageUrl: 'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_4x3.jpg'),
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067__340.png'),
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://images.unsplash.com/photo-1612441804231-77a36b284856?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bW91bnRhaW4lMjBsYW5kc2NhcGV8ZW58MHx8MHx8&w=1000&q=80'),
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://capturelandscapes.com/wp-content/uploads/2020/11/Fundamentals-of-landscape-photography-gallery-2.jpg'),
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://petapixel.com/assets/uploads/2022/08/fdfs11-800x533.jpg'),
+           
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://petapixel.com/assets/uploads/2022/08/fdfs18-800x533.jpg'),
+           
+           
+            SizedBox(height: 10,),
+            CustomCardType2(imageUrl: 'https://d150u0abw3r906.cloudfront.net/wp-content/uploads/2021/10/image2-2-1024x649.png'),
+           
             ],
         ));
   }
--- lib/widgets/custom_card_type2.dart
+++ lib/widgets/custom_card_type2.dart
@@ -1,36 +1,33 @@
 import 'package:flutter/material.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({super.key});
+  final String imageUrl;
+  const CustomCardType2({super.key, required this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      clipBehavior: Clip.antiAlias,// Evita que los elementos se salgan, como las imagenes
+      clipBehavior: Clip
+          .antiAlias, // Evita que los elementos se salgan, como las imagenes
       elevation: 10,
 
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage(
-                'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_4x3.jpg'),
-            placeholder: AssetImage('assets/images/jar-loading.gif'),
-           // width entero
-            width: double.infinity, 
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/images/jar-loading.gif'),
+            // width entero
+            width: double.infinity,
             height: 230,
             // llena toda la imagen
             fit: BoxFit.cover,
             // animation view image
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
           Container(
             alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(
-              right: 20,
-              top: 10,
-              bottom: 10
-            ),
+            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
             child: const Text('un hermoso paisaje'),
           )
         ],
