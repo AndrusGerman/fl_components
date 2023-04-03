dc68aff0669514dbc4389eeb448d874f7e96e957
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 23:28:13 -0400

    text button theme

--- lib/theme/app_theme.dart
+++ lib/theme/app_theme.dart
@@ -5,9 +5,17 @@ class AppTheme {
 
   static final ThemeData lightTheme = ThemeData.light().copyWith(
     primaryColor: primaryColor,
+
     appBarTheme: const AppBarTheme(
       color: primaryColor,
       elevation: 0,
     ),
+
+   
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        foregroundColor: primaryColor
+      )
+    )
   );
 }
