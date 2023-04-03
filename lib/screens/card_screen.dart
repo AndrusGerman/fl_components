500ed9a74f8a8aa0306474e4d4b85eba618f6251
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 23:40:49 -0400

    add card image

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:fl_components/widgets/custom_card_type1.dart';
+import 'package:fl_components/widgets/custom_card_type2.dart';
 import 'package:flutter/material.dart';
 
 class CardScreen extends StatelessWidget {
@@ -15,7 +16,11 @@ class CardScreen extends StatelessWidget {
             horizontal: 20,
             vertical: 10,
           ),
-          children: const [CustomCardType1()],
+          children: const [
+            CustomCardType1(),
+            SizedBox(height: 10,),
+            CustomCardType2(),
+            ],
         ));
   }
 }
---
+++ lib/widgets/custom_card_type2.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomCardType2 extends StatelessWidget {
+  const CustomCardType2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 10,
+      child: Column(
+        children: const [
+          FadeInImage(
+            image: NetworkImage(
+                'https://i.natgeofe.com/n/2a832501-483e-422f-985c-0e93757b7d84/6_4x3.jpg'),
+            placeholder: AssetImage('assets/images/jar-loading.gif'),
+          )
+        ],
+      ),
+    );
+  }
+}
