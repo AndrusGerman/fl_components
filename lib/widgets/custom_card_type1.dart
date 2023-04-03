5ebed7f17f694f2e8da0005f36d895cf8f250dd2
AndrusGerman <andrusdiazaleman@gmail.com>
2023-04-02 23:21:00 -0400

    card list

--- lib/widgets/custom_card_type1.dart
+++ lib/widgets/custom_card_type1.dart
@@ -10,16 +10,28 @@ class CustomCardType1 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Card(
       child: Column(
-        children: const [
-          ListTile(
+        children: [
+          const ListTile(
             leading: Icon(
               Icons.photo_album,
               color: AppTheme.primaryColor,
             ),
             title: Text('Soy Un titulo'),
-            subtitle: Text('Culpa aute aute in quis adipisicing veniam ipsum.'),
+            subtitle: Text('Commodo elit aute ipsum veniam reprehenderit voluptate Lorem Lorem proident sit dolore consequat nisi. Nisi aliqua ea esse reprehenderit et pariatur sunt velit dolore dolore proident laborum Lorem incididunt. Officia officia nisi excepteur Lorem et aliquip dolor in elit labore. Duis sunt duis reprehenderit cupidatat. Do labore consectetur irure officia exercitation nostrud sunt aliqua eu duis id dolore. Consectetur aliquip exercitation consectetur magna do pariatur Lorem pariatur commodo eiusmod duis.'),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 5),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                TextButton(onPressed: () {}, child: const Text('Cancel')),
+                TextButton(onPressed: () {}, child: const Text('ok')),
+              ],
+            ),
           )
         ],
+
       ),
     );
   }
